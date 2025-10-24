1ccab396ad4a2d2f69c661f62ac751fe39a70c39
eng-ahmad-alhamwi <linaahmadabd@gmail.com>
2025-10-24 10:50:44 +0300

    commit vercert

--- resources/js/services/api.ts
+++ resources/js/services/api.ts
@@ -2,7 +2,7 @@ import { AboutData, AboutFormData, AchievData, AchievFormData, CategoryData, Cat
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://ahmad-alhamwi.vercel.app/api',
   withCredentials: true,
   headers: {
     'Accept': 'application/json',
